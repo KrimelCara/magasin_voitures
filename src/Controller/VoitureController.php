87f748504c4509f51563232d6e68687865f2d819
Krimel <abderr-27@outlook.fr>
2022-05-20 15:23:48 +0200

    ajout des tables en base de donné

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Voiture;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VoitureController extends AbstractController
 {
-    #[Route('/voiture', name: 'app_voiture')]
+    #[Route('/', name: 'app_voiture')]
     public function index(): Response
     {
-        return new Response("Hello World");
-        return $this->json(['message' => 'Hello World']);
- 
+      
+
     }
+
 }
